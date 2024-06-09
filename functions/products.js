f97c06bfd0c49d940c96dc8b3f637f4c4be1471a
Carlos Ortega <carlosortega77@gmail.com>
2024-06-08 23:27:46 -0400

    console.log

--- functions/products.js
+++ functions/products.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFile = path.join(__dirname, '../_products/products.json');
+const productsFile = path.join(__dirname, '../public/_products/products.json');
+console.log('Products file path:', productsFile);
 
 exports.handler = async function(event, context) {
     try {
+        console.log('Attempting to read products file');
         const products = JSON.parse(fs.readFileSync(productsFile, 'utf8'));
+        console.log('Products loaded successfully');
         return {
             statusCode: 200,
             body: JSON.stringify(products),
