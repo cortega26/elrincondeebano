5ab102be14c8e8ae11640cd7c3eb13c6988ce245
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2025-06-21 17:06:45 -0400

    Revert local asset hosting to avoid binary files

---
+++ test/serviceWorker.utils.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { addTimestamp, isCacheFresh } = require('../service-worker.js');
+
+(async () => {
+  const resp = new Response('data');
+  const stamped = await addTimestamp(resp.clone(), 'static');
+  assert.ok(stamped.headers.get('sw-timestamp'), 'timestamp should be added');
+  assert.strictEqual(stamped.headers.get('cache-type'), 'static');
+  assert.strictEqual(isCacheFresh(stamped, 'static'), true, 'fresh response should be fresh');
+
+  const headers = new Headers(stamped.headers);
+  const past = Date.now() - 2 * 24 * 60 * 60 * 1000; // older than cache duration
+  headers.set('sw-timestamp', past.toString());
+  const oldResp = new Response('old', { headers });
+  assert.strictEqual(isCacheFresh(oldResp, 'static'), false, 'stale response should be stale');
+
+  console.log('All tests passed');
+})();
